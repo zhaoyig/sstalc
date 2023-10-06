265877131387db6937f605eee6573efb1f7f797e
August <august0086@outlook.com>
2023-10-05 20:22:59 -0400

    read from file

--- src/parser/main.ml
+++ src/parser/main.ml
@@ -1,6 +1,29 @@
 open Tal
 
+type location =
+  | Location of Lexing.position * Lexing.position (** delimited location *)
+  | Nowhere (** no location *)
+
+exception Error of (location * string * string)
+
+
 let parse (s : string) : instruction_seq =
   let lexbuf = Lexing.from_string s in
   let instruction_seq = Parser.prog Lexer.read lexbuf in
   instruction_seq
+
+let parse_file fn =
+  try
+    let fh = open_in fn in
+    let lex = Lexing.from_channel fh in (* Create a lexbuf *)
+    lex.Lexing.lex_curr_p <- {lex.Lexing.lex_curr_p with Lexing.pos_fname = fn};
+    try
+      let terms = Parser.prog Lexer.read lex in
+      close_in fh;
+      terms
+    with
+      (* Close the file in case of any parsing errors. *)
+      Error err -> close_in fh ; raise (Error err)
+  with
+    (* Any errors when opening or closing a file are fatal. *)
+    Error err -> () ; raise (Error err)
