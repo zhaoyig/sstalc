7cd3b04273e27aebc102a0cff58807f6fb1833a0
August <august0086@outlook.com>
2023-10-15 20:31:38 -0400

    create excutable

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,17 @@
-let () = print_endline "Hello, World!"
+open Arg
+
+let usageMsg = "stalc <file1> -o <output> -r "
+
+let inputFile = ref ""
+let outputFile = ref ""
+
+let flagRun = ref false
+
+let speclist = [
+  ("-o", Set_string outputFile, "Set the output file name");
+  ("-r", Set flagRun, "run the file");
+]
+
+let () = 
+  Arg.parse speclist (fun file -> inputFile := file) usageMsg;
+  Codegen.compileFile "test/a.stal";
